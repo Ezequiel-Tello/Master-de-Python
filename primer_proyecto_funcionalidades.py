2ab0120aaff49b4e2d51904343676e979d44f155
Ezequiel-Tello <ezequieltelloaguilar@hotmail.com>
2026-05-03 14:27:06 -0300

    Proyecto 2 en .py
    Aplicación directa al código:
    En este 2do proyecto usamos funcionalidades para formatear algunos str del programa creado en el 1er proyecto y lo mejoramos visualmente con separaciones básicas de "-".

--- primer_proyecto_funcionalidades.py
+++ primer_proyecto_funcionalidades.py
@@ -4,13 +4,17 @@ This module provides functions for [describe purpose of the script].
 # 1: Mostramos mensaje de bienvenida
 print("¡Bienvenido a tu software administrativo personal!")
 # 2 y 3: Pedimos al usuario que ingrese los datos requeridos y almacenamos los valores en variables
-producto = input("Ingrese el nombre del producto: ")
+producto = input("Ingrese el nombre del producto: ").title().strip()
 cantidad = int(input("Ingrese la cantidad en stock: "))
 precio = float(input("Ingrese el precio por unidad: $"))
-valor_total = (cantidad * precio)
-# 4 vamos a mostrar todo el inventario con la informacion de este
+valor_total = cantidad * precio
+# 4 Vamos a mostrar todo el inventario con la informacion de este
+# 5 Use f-string para formateare el valor de las variables
 print("---Inventario inicial---")
-print("Nombre del producto: ", (producto))
-print("Cantidad en stock: ", (cantidad))
-print("Precio unitario: $", (precio))
-print("valor de inventario inicial: $", (valor_total))
+print("-" * 30)
+print(f"Nombre del producto               : {producto}")
+print(f"Cantidad en stock                 : {cantidad}")
+print(f"Precio unitario                   : ${precio:.2f}")
+print(f"valor de inventario inicial       : ${valor_total:.2f}")
+print("-" * 30)
+# 6 Terminamos el proyecto numero 2 usando "print" con guiones como separación para una mejora visual
